f1ec24cbdd9479675cf4a816e93782771116bdd4
sarahsaleh <sarahtsige@gmail.com>
2020-02-02 23:54:32 -0500

    added logic for multiple war scenarios

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,4 @@
 console.log("hello!");
-// The deck before it’s distributed to the players ( let deck = [])
-
-//function deck() =  {
-//take two arrays suits and rank and iterate through the loops to creat a deck
-//and put them into "deck"
 
 suit = ["clubs", "diamonds", "spades", "hearts"]
 //rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
@@ -14,9 +9,9 @@ function startGame() {
     buildDeck();
     shuffle();
     deal();
-//while (playerOneDeck.length < deck.length || playerTwoDeck.length < deck.length)
+    //while (playerOneDeck.length < deck.length || playerTwoDeck.length < deck.length)
     while (playerOneDeck.length !== 0 && playerTwoDeck.length !== 0) {
-    	round()
+        round()
     }
 }
 
@@ -33,10 +28,7 @@ function buildDeck() {
     }
 }
 
-//Math.random => to shuffle cards
-
 //Shuffling the deck - (Fisher-Yates Algorithm)
-
 function shuffle() {
     for (i = deck.length - 1; i > 0; i--) {
         j = Math.floor(Math.random() * i);
@@ -46,7 +38,6 @@ function shuffle() {
     }
 }
 
-
 //DEALING THE CARDS
 // The first player’s cards (let playerOneDeck = [])
 let playerOneDeck = []
@@ -54,7 +45,6 @@ let playerOneDeck = []
 let playerTwoDeck = []
 
 // if index of card is even, push to playerTwo array, otherwise, push to playerOne
-
 function deal() {
     for (i = 0; i < deck.length; i++) {
         if (i % 2 === 0) {
@@ -65,35 +55,16 @@ function deal() {
     }
 }
 
-//*******doesn't empty the deck array********
-
-
-
-
-
-// The first player’s card being played for that particular turn (let playerOneCard={})
-
-// The second player’s card being played for that particular turn (let playerTwoCard={})
-
-//each round == object  
-
-//EACH ROUND BEING PLAYED
-
-
-
-
-
 function cardTurn() {
     //gameBoard.push(playerOneDeck[i], playerTwoDeck[i])
     console.log('Player1 has a ' + playerOneDeck[0].value + ' of ' + playerOneDeck[0].suit)
     console.log('Player2 has a ' + playerTwoDeck[0].value + ' of ' + playerTwoDeck[0].suit)
 }
 
-
-
-//if (p1 card value > p2 card value)  => push both cards to th end of p1deck array,  else if (p2 card value > p1) =>  push to end of Player 2 deck  else
-
-
+/* 
+	if (p1 card value > p2 card value)  => push both cards to th end of p1deck array,  
+	else if (p2 card value > p1) =>  push to end of Player 2 deck  else 
+*/
 function round() {
     cardTurn()
     let loosersCard, winnersCard;
@@ -102,8 +73,8 @@ function round() {
         playerOneDeck.push(loosersCard[0]);
         winnersCard = playerOneDeck.splice(0, 1)
         playerOneDeck.push(winnersCard[0]);
-        console.log("Player 1 wins this round! Player 1 has " + playerOneDeck.length + "cards and Player 2 has " + playerTwoDeck.length + " cards.")
-    } else if (playerTwoDeck[i] > playerOneDeck[i]) {
+        console.log(`Player 1 wins this round! Player 1 has ${playerOneDeck.length} cards and Player 2 has ${playerTwoDeck.length} cards.`)
+    } else if (playerTwoDeck[0].value > playerOneDeck[0].value) {
         loosersCard = playerOneDeck.splice(0, 1);
         playerTwoDeck.push(loosersCard[0]);
         winnersCard = playerTwoDeck.splice(0, 1)
@@ -111,9 +82,6 @@ function round() {
         console.log("Player 2 wins this round! Player 1 has " + playerOneDeck.length + "cards and Player 2 has " + playerTwoDeck.length + " cards.")
     } else {
         console.log("It's a tie....War has been declared!!")
-        war()
-    }
-    function war() {
         if (playerOneDeck[3].value > playerTwoDeck[3].value) {
             let warWinnersCards, warLoosersCards
             warLoosersCards = playerTwoDeck.splice(0, 4);
@@ -130,43 +98,30 @@ function round() {
         } else {
             console.log("It's another tie....War has been declared again!!")
         }
-
     }
+}
+
+/*
+	Should compare the fourth card in the array and if tie every fourth card after that until on  player take all the cards.
+*/
+function war() {
+	while (playerOneDeck[])
+	if (playerOneDeck[3].value > playerTwoDeck[3].value) {
+            let warWinnersCards, warLoosersCards
+            warLoosersCards = playerTwoDeck.splice(0, 4);
+            playerOneDeck.push(...warLoosersCards);
+            warWinnersCards = playerOneDeck.splice(0, 4)
+            playerOneDeck.push(...warWinnersCards);
+            console.log("Player 1 wins this round! Player 1 has " + playerOneDeck.length + "cards and Player 2 has " + playerTwoDeck.length + " cards.")
+        } else if (playerTwoDeck[3].value > playerOneDeck[3].value) {
+            warLoosersCards = playerOneDeck.splice(0, 4);
+            playerTwoDeck.push(warLoosersCards[0]);
+            warWinnersCards = playerTwoDeck.splice(0, 4)
+            playerTwoDeck.push(warWinnersCards[0]);
+            console.log("Player 1 wins this round! Player 1 has " + playerOneDeck.length + "cards and Player 2 has " + playerTwoDeck.length + " cards.")
+
+}
 
 
 
-
-
-    
-
-
-
-// Variables:
-//firstPlayerDeck = [];
-// secondPlayerDeck = [];
-// cardDeck = [];
-// spoilsOfWar = [];
-// warDeclared = boolean;
-// winnerOfRound = null;
-
-// Functions:
-// startGame()
-// shuffle()
-// distributeCards()
-// conductTurn()
-// addCardsInPlay()
-// determineWinner()
-// prepareForWar()
-// assignWinnerCards()
-// receiveSpoilsOfWar()
-// finishTurn()
-// endGame()
-
-
-
-// suit = ["club", "diamond", "spade", "heart"]
-// value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
-
-// for (i = )
-
+startGame();
